9b896d80bbe761b16113521f2ec8bd1b0c2b698c
kiinRain <kiinRain@163.com>
2021-11-01 22:07:12 +0800

    1

---
+++ src/components/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+ <h2>{{name}}</h2>
+ <h2>{{age}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+ data() {
+   return {
+     name:'张三',
+     age:'44'
+   }
+ },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
